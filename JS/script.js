ef8ec75cf6fe6d2725f196d97c555f84f0c3a4c0
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-15 15:54:38 +0100

    added array of objects

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,50 @@
 console.log('HELLO WORLD')
+
+// CREO UN ARRAY DI OGGETTI
+const teamMembers = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg'
+
+    },
+
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: ''
+
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg'
+
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg'
+
+    },
+
+    {
+        name: 'Scott Estrada ',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg'
+
+    },
+
+    {
+        name: 'Barbara Ramos',
+        role: '	Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg'
+
+    }
+]
+
+console.log(teamMembers)
+
+// STAMPO IN CONSOLE LE CHIAVI DEGLI OGGETTI NELL'ARRAY
